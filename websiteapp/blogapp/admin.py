e881c83c325b5cf823071a5e9787eb6ac0aa138c
JBthePenguin <jblab976@gmail.com>
2019-02-15 22:14:31 +0100

    add models for blogapp
    	nouveau fichier : uploads/blog/django.png
    	modifié :         websiteapp/blogapp/admin.py
    	modifié :         websiteapp/blogapp/models.py
    	modifié :         websiteapp/portfolioapp/admin.py
    	modifié :         .gitignore
    "add models in blogapp"

--- websiteapp/blogapp/admin.py
+++ websiteapp/blogapp/admin.py
@@ -1,3 +1,65 @@
+from django import forms
 from django.contrib import admin
+from websiteapp.blogapp.models import BlogCategory, Post, Comment
 
-# Register your models here.
+
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name_en', 'name_fr',)
+    list_display = ('name_en', 'name_fr',)
+
+
+class CategoryModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s - %s" % (obj.name_en, obj.name_fr)
+
+
+class PostAdminForm(forms.ModelForm):
+    category = CategoryModelChoiceField(queryset=BlogCategory.objects.all())
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+    search_fields = (
+        'title_en', 'title_fr', 'category__name_fr', 'category__name_en')
+    list_display = ('title_en', 'title_fr', 'get_category', 'date')
+
+    def get_category(self, obj):
+        return "%s - %s" % (obj.category.name_en, obj.category.name_fr)
+
+    get_category.admin_order_field = 'category'
+    get_category.short_description = 'Category'
+
+
+class PostModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s - %s" % (obj.title_en, obj.title_fr)
+
+
+class CommentAdminForm(forms.ModelForm):
+    post = PostModelChoiceField(queryset=Post.objects.all())
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    form = CommentAdminForm
+    search_fields = (
+        'author_name', 'author_email', 'post__title_fr',
+        'post__title_en', 'valid')
+    list_display = (
+        'valid', 'get_post', 'author_name', 'author_email', 'date')
+
+    def get_post(self, obj):
+        return "%s - %s" % (obj.post.title_en, obj.post.title_fr)
+
+    get_post.admin_order_field = 'post'
+    get_post.short_description = 'Post'
--- websiteapp/blogapp/models.py
+++ websiteapp/blogapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogCategory(models.Model):
+    name_en = models.CharField(db_index=True, max_length=255)
+    name_fr = models.CharField(db_index=True, max_length=255)
+
+
+class Post(models.Model):
+    category = models.ForeignKey(
+        BlogCategory, db_index=True, default='', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog', default='')
+    title_en = models.CharField(db_index=True, max_length=255)
+    title_fr = models.CharField(db_index=True, max_length=255)
+    content_en = models.TextField()
+    content_fr = models.TextField()
+    date = models.DateField()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, db_index=True, default='', on_delete=models.CASCADE)
+    author_name = models.CharField(db_index=True, max_length=255)
+    author_email = models.CharField(db_index=True, max_length=255)
+    text = models.CharField(db_index=True, max_length=255)
+    date = models.DateTimeField(db_index=True, auto_now_add=True)
+    valid = models.BooleanField(default=False)
